40d40008f182a6bb1c80aa61f810c12c462688ad
APq198 <ntapqmail@gmail.com>
2023-10-16 17:52:11 +0200

    aa2

--- stepping.cc
+++ stepping.cc
@@ -14,6 +14,7 @@ void MySteppingAction::UserSteppingAction(const G4Step * step)
 	//fEventAction->AddEdep(edep);
 	//G4cout << "something2_step" << G4endl;
 	static G4int cntr = 0;
-	G4cout << cntr << ") ..." << G4endl;
+	//G4cout << cntr << ") ..." << G4endl;
+	cntr ++;
 }
 
