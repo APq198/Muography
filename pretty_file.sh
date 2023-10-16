e608230373edcab9e4653743fd3397ab2821b488
APq198 <ntapqmail@gmail.com>
2023-10-16 23:00:15 +0200

    pretty 3

--- pretty_file.sh
+++ pretty_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-input="/path/to/txt/file"
+input="./$1"
 while IFS= read -r var
 do
   #
-  # if value of $var starts with #, ignore it
+  # if value of $var starts with "a", ignore it
   #
   [[ $var =~ ^a.* ]] && continue
   echo "$var" >> file2.txt
