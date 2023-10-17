a6831ea4b36b18f6cc79de918fdbf6295e649cc5
APq198 <ntapqmail@gmail.com>
2023-10-17 14:15:21 +0200

    aa20

--- ultimate_pretty_fule.sh
+++ ultimate_pretty_fule.sh
@@ -3,13 +3,13 @@ numOfThreads=4
 prefix="G4W_"
 suffix="_$1"
 filename="end_output11.txt"
-for i in {1..4}
+i=0
+while [ $i -lt $numOfThreads ]	
 do
 	##
 	##
 	##
-	echo $i
-	let j=$i-1
 	input="./$prefix$i$suffix"
 	echo $input
+	((i++))
 done
