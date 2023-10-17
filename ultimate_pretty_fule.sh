e892c0f33779a27dce36ab20f1979c741e83b73d
APq198 <ntapqmail@gmail.com>
2023-10-17 14:10:30 +0200

    aa17

--- ultimate_pretty_fule.sh
+++ ultimate_pretty_fule.sh
@@ -3,11 +3,12 @@ numOfThreads=4
 prefix="G4W_"
 suffix="_$1"
 filename="end_output11.txt"
-for i in {0..$numOfThreads-1}
+for i in {1..$numOfThreads}
 do
 	##
 	##
 	##
+	let j=$i-1
 	input="./$prefix$i$suffix"
 	echo $input
 done
