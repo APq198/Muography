19a7dbbdda25c1847dcb591b069b67e3b0c4f112
APq198 <ntapqmail@gmail.com>
2023-10-17 14:55:11 +0200

    aa23

--- gitpull_and_try.sh
+++ gitpull_and_try.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
 git reset --hard HEAD
 git pull
+cmake ..
 make 
+. remove_files.sh output11.txt
+###rm end_output11.txt
 ./main
-. pretty_file.sh G4W_0_output11.txt
+. ultimate_pretty_file.sh output11.txt
+. find_muons.sh end_output11.txt
 echo "================"
 echo "===== Done ====="
 echo "================"
