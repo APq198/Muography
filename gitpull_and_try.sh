d82699aca2af489f80cf9628cbc498a4d9dcbeb9
APq198 <ntapqmail@gmail.com>
2023-10-18 12:26:14 +0200

    aa35

--- gitpull_and_try.sh
+++ gitpull_and_try.sh
@@ -6,8 +6,8 @@ make
 . remove_files.sh output11.txt
 ###rm end_output11.txt
 ./main
-. ultimate_pretty_file.sh output11.txt
-. find_muons.sh end_output11.txt
+###. ultimate_pretty_file.sh output11.txt
+###. find_muons.sh end_output11.txt
 echo "================"
 echo "===== Done ====="
 echo "================"
--- stepping.cc
+++ stepping.cc
@@ -20,6 +20,7 @@ void MySteppingAction::UserSteppingAction(const G4Step * step)
 	cntr ++;
 	//G4cout << "a";
 	//std::cout << std::flush;
-	G4cout << "a";
+	//G4cout << "a";
+	std::cout << "a";
 }
 
