d79a31a62edd7856778d8921bdae0ddb05167323
APq198 <ntapqmail@gmail.com>
2023-11-08 21:43:25 +0100

    r27

--- stepping.cc
+++ stepping.cc
@@ -52,13 +52,14 @@ void MySteppingAction::UserSteppingAction(const G4Step * step)
 	if (particleName.compare(0, 2, "mu")==0)
 	{
 		G4ThreeVector pos = track->GetPosition();
-		if ( pos[1] < -Y_WORLD_VAL+1*m )
+		if ( pos[1] < -Y_WORLD_VAL+30*m )
 		{
 			//fEventAction->numIncidentMuons += 1;
 			G4double energy = track->GetTotalEnergy();
 			G4cout 	<< G4endl << particleName << ", "
 					<< energy
 					<< G4endl;
+			track->SetTrackStatus(fStopAndKill);
 		}
 	}
 
