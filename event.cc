658c849720235177f27538c3d2735d63f755a939
APq198 <ntapqmail@gmail.com>
2023-11-08 19:27:33 +0100

    r22

--- event.cc
+++ event.cc
@@ -25,7 +25,7 @@ void MyEventAction::BeginOfEventAction(const G4Event * event)
 
 void MyEventAction::EndOfEventAction(const G4Event * event)
 {
-	// G4cout << "end of event; " << G4endl;
+	G4cout << G4endl << "end of event; " << G4endl;
 	#ifdef SEARCHING_FOR_WINDOW
 		G4cout << G4endl << numIncidentMuons << " # counted Muons" << G4endl;
 	#endif
