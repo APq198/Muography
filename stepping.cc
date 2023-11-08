e8abe2ac4e01c7e8de4c83f704f5f5cdec03a73d
APq198 <ntapqmail@gmail.com>
2023-11-08 22:46:24 +0100

    r45

--- stepping.cc
+++ stepping.cc
@@ -64,7 +64,7 @@ void MySteppingAction::UserSteppingAction(const G4Step * step)
 			// 	G4cout << "u";
 			track->SetTrackStatus(fStopAndKill);
 		}
-		G4cout << "mu!" << G4endl;
+		//G4cout << "mu!" << G4endl;
 		
 	}
 
