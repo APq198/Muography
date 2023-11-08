f5bd3578fc2b6426060846b83d1071ea6c79360c
APq198 <ntapqmail@gmail.com>
2023-11-08 22:38:44 +0100

    r43

--- event.cc
+++ event.cc
@@ -20,7 +20,7 @@ void MyEventAction::BeginOfEventAction(const G4Event * event)
 	 */
 	// G4cout << "begin of event; " << G4endl;
 	numIncidentMuons = 0;
-	for (int i =0; i<1000; i++)
+	for (int i =0; i<10000; i++)
 		G4cout << "u";
 	G4cout << G4endl;
 }
